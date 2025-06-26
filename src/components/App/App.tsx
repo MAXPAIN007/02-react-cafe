5c8227119566e9806c0ef1b2f65411b5c79d533c
MAXPAIN007 <krmetalist@gmail.com>
2025-06-27 01:46:53 +0200

    HW2React is Done

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,6 +1,6 @@
 import css from "./App.module.css";
 import CafeInfo from "../CafeInfo/CafeInfo";
-import VoteOptions from "../Vote-options/VoteOptions";
+import VoteOptions from "../VoteOptions/VoteOptions";
 import VoteStats from "../Vote-stats/VoteStats";
 import Notification from "../Notification/Notification";
 
