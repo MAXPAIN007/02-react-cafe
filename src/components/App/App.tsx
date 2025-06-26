fa0a0bafdebd6261f057d887659fdf4c8edd8162
MAXPAIN007 <krmetalist@gmail.com>
2025-06-27 01:29:47 +0200

    HW2React is Done

---
+++ src/components/App/App.tsx
@@ -0,0 +1,56 @@
+import css from "./App.module.css";
+import CafeInfo from "../Cafe-info/CafeInfo";
+import VoteOptions from "../Vote-options/VoteOptions";
+import VoteStats from "../Vote-stats/VoteStats";
+import Notification from "../Notification/Notification";
+
+import { useState } from "react";
+import type { Votes, VoteType } from "../../types/votes";
+
+export default function App() {
+  const [votes, setVotes] = useState<Votes>({
+    good: 0,
+    neutral: 0,
+    bad: 0,
+  });
+
+  const handleVote = (type: VoteType) => {
+    setVotes({
+      ...votes,
+      [type]: votes[type] + 1,
+    });
+  };
+
+  const resetVotes = () => {
+    setVotes({
+      good: 0,
+      neutral: 0,
+      bad: 0,
+    });
+  };
+
+  const totalVotes = votes.good + votes.neutral + votes.bad;
+  const positiveRate = totalVotes
+    ? Math.round((votes.good / totalVotes) * 100)
+    : 0;
+
+  return (
+    <div className={css.app}>
+      <CafeInfo />
+      <VoteOptions
+        onVote={handleVote}
+        onReset={resetVotes}
+        canReset={totalVotes > 0 ? true : false}
+      />
+      {totalVotes ? (
+        <VoteStats
+          votes={votes}
+          totalVotes={totalVotes}
+          positiveRate={positiveRate}
+        />
+      ) : (
+        <Notification />
+      )}
+    </div>
+  );
+}
